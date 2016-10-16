62419071235ab4a3af1ce6a1e184c16c24ee8f42
billgeo <bmnelson@linz.govt.nz>
2016-10-17 10:44:34 +1300

    fix Shift Longitude repeatability

--- sql/setup_meshblock.sql
+++ sql/setup_meshblock.sql
@@ -1,7 +1,7 @@
 alter table admin_bdys.meshblock add primary key (code);
 alter table admin_bdys.meshblock_concordance add primary key (meshblock);
 
-update admin_bdys.meshblock set shape = ST_Shift_Longitude(shape);
+update admin_bdys.meshblock set shape = ST_Shift_Longitude(shape) where ST_XMin(shape) < 0;
 
 SELECT table_version.ver_enable_versioning('admin_bdys', 'meshblock');
 SELECT table_version.ver_enable_versioning('admin_bdys', 'meshblock_concordance');
--- sql/setup_nz_locality.sql
+++ sql/setup_nz_locality.sql
@@ -1,6 +1,6 @@
 alter table admin_bdys.nz_locality add primary key (id);
 create index on admin_bdys.suburb_alias (locality_id);
 
-update admin_bdys.nz_locality set shape = ST_Shift_Longitude(shape);
+update admin_bdys.nz_locality set shape = ST_Shift_Longitude(shape) where ST_XMin(shape) < 0;
 
 SELECT table_version.ver_enable_versioning('admin_bdys', 'nz_locality');
